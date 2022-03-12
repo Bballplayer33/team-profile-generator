94a0746670275f1c7d806f544fc4584997183f6b
Earl Adams <earladams2000@gmail.com>
2022-03-12 13:17:18 -0500

    create templates to render html

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "");
+const render = require("./lib/createHTML");
 
 let employeeArray = [];
 
